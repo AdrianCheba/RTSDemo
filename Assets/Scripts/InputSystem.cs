485b20e65067f1651aa315c18f85a46d4390853b
Adrian Cheba <adiche@wp.pl>
2025-09-15 21:41:23 +0200

    Refactor UnitSelectionManeger

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -5,6 +5,42 @@ namespace Scripts
 {
     public class InputSystem : MonoBehaviour
     {
-       
+        UnitSelectionManager _unitSelectionManager;
+
+        [SerializeField]
+        InputActionAsset _inputActions;
+
+        InputActionMap _map;
+
+        InputAction _movement;
+
+        private void Awake()
+        {
+            _map = _inputActions.FindActionMap("Player");
+            _unitSelectionManager = GetComponent<UnitSelectionManager>();
+            Initialization();
+        }
+
+        private void OnEnable()
+        {
+            _map.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _map.Disable();
+        }
+
+        private void Initialization()
+        {
+            _movement = _map.FindAction("Movement");
+            _movement.performed += _ =>
+            {
+                foreach (GameObject unit in _unitSelectionManager._unitsList)
+                {
+                    unit.GetComponent<UnitMovement>().OnMovement();
+                }
+            };
+        }
     }
 }
--- Assets/Scripts/UnitSelectionManager.cs
+++ Assets/Scripts/UnitSelectionManager.cs
@@ -1,10 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Scripts
 {
-
     public class UnitSelectionManager : MonoBehaviour
     {
         public static UnitSelectionManager Instance { get; set; }
@@ -12,10 +10,8 @@ namespace Scripts
         [SerializeField]
         internal List<GameObject> _unitsList;
 
-        [SerializeField]
-        InputActionAsset _inputActions;
+        internal List<GameObject> _unitsList2;
 
-        InputAction _movement;
         private void Awake()
         {
             if (Instance != null && Instance != this)
@@ -26,31 +22,6 @@ namespace Scripts
             {
                 Instance = this;
             }
-
-            _movement = _inputActions.FindActionMap("Player").FindAction("Movement");
-        }
-
-        private void OnEnable()
-        {
-            _movement.Enable();
-            _movement.performed += OnMovement;
-        }
-
-        private void OnDisable()
-        {
-            _movement.performed -= OnMovement;
-            _movement.Disable();
-        }
-
-        private void OnMovement(InputAction.CallbackContext context)
-        {
-            _movement.performed += _ =>
-            {
-                foreach (GameObject unit in _unitsList)
-                {
-                    unit.GetComponent<UnitMovement>().OnMovement();
-                }
-            };
         }
     }
 }
